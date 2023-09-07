d5a38e6deb309d67d76179b98a929a33bc1ff145
sjanorkar <swapnil.janorkar@gmail.com>
2023-09-07 15:19:01 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+
+curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-440.0.0-linux-x86_64.tar.gz
+tar -xvf google-cloud-cli-440.0.0-linux-x86_64.tar.gz
+#./google-cloud-sdk/install.sh --quite
+#
+## Install the gke-gcloud-auth-plugin
+#gcloud components install kubectl
+#gcloud components install kubernetes-helm
+#gcloud components install gke-gcloud-auth-plugin
